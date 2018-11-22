8899691b9ee0a7d73593e01d19092214754bdd21
gichimu mwai <gichimumwai1@mail.com>
2018-11-22 23:42:36 +0300

    the bot works!

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 console.log("the bot is working");
 
-const twit = require("twit");
+var Twit = require('twit');
 
-var T = new Twit({
-  consumer_key:         '10EPs4fvErw3TCPuUB2NDHnjQ',
-  consumer_secret:      'zx3AOMYl9hvasROzRi8lNSvllAyl26YVFHKVWwk4jD3i4OlRQL',
-  access_token:         '1065539409204977665-7AzkskIgl5RdtyabzeqOhXBcCpsxb5 ',
-  access_token_secret:  'V4Nbys4rzUmcWA04CcH3QwBkRxn4KjkLue3nzOfhAPoIg',
-  timeout_ms:           60*1000,
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
+var config = require('./config');
+
+var T = new Twit(config);
+
+var params =  {
+   q: 'rainbow',
+   count: 5
+  }
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+  console.log(data);
+}
