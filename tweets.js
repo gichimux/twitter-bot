cd2adcbbf596189de4c2ed6fe3cd0fc37c7f9b04
gichimu mwai <gichimumwai1@mail.com>
2018-11-23 15:05:53 +0300

    added tweet file!

---
+++ tweets.js
@@ -0,0 +1,11 @@
+module.exports = [
+  'A plateau is the highest form of flattery.'
+
+ ,'It’s hard to explain puns to kleptomaniacs because they always take things literally.'
+
+ ,'Time flies like an arrow, fruit flies like a banana.'
+
+ ,'A soldier survived mustard gas in battle, and then pepper spray by the police\. He\'s now a seasoned veteran'
+
+ ,'I hate Russian dolls\.\.\.so full of themselves'
+]
